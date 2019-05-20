2e7e48afa829e06cedf515dd59a4c75be25d313f
Jenee Geer <jenee@plie.codes>
2019-05-20 11:33:51 -0500

    add table for search results

--- app/controllers/shortlinks_controller.rb
+++ app/controllers/shortlinks_controller.rb
@@ -24,16 +24,14 @@ class ShortlinksController < ApplicationController
 
   def index
     Rails.logger.info("Now in index action")
-    if params[:email] && params[:shortened]
-      Rails.logger.info("Now in the if of the index action")
-      @shortlinks = Shortlink.where('shortened = (?)', params[:shortened]).where('email = (?)', params[:email])
-    elsif params[:email] && params[:original]
-      Rails.logger.info("Now in the elsif of the index action")
-      @shortlinks = Shortlink.where('original = (?)', params[:original]).where('email = (?)', params[:email])
-    else
-      Rails.logger.info("Now in the else of the index action")
-      flash[:info] = 'Your link was not found. Be sure to enter your correct email and try again.'
-      redirect_to :back
+    if params[:email]
+      if params[:shortened]
+        Rails.logger.info("Now in the if of the index action")
+        @shortlinks = Shortlink.where('shortened = (?)', params[:shortened]).where('email = (?)', params[:email])
+      else
+        Rails.logger.info("Now in the else of the index action")
+        @shortlinks = Shortlink.where('original = (?)', params[:original]).where('email = (?)', params[:email])
+      end
     end
   end
 
